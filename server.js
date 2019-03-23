f7eb1d9e3582b705f7de987a7ebe2aea2c97ed3a
Blaney83 <professionallaney@gmail.com>
2019-03-22 20:31:32 -0700

    Playground testing/

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const router = require("express").Router();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+// router.get("/", (req, res)=>{
+//     res.render("./public/index.html")
+// })
+
+// Start the API server
+const server = app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
